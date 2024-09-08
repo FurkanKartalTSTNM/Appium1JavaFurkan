f15fbec3a243cfbb0c8d4420eb0a3fb4b98a78b3
Furkan Kartal <Furkan.kartal@testinium.com>
2024-09-08 16:29:47 +0300

    deneme

--- src/test/java/Testasd.java
+++ src/test/java/Testasd.java
@@ -14,7 +14,8 @@ public class Testasd extends HookImpl {
 
         step.waitBySecond(3);
         //wait until element is visible
-        step.clickByKey("com.gratis.android:id/nav_graph_campaign");
+        step.existElement("com.gratis.android:id/nav_graph_campaign");
+        step.swipe("down");
     }
 
 }
