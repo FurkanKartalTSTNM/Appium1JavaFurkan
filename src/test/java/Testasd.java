2576ebeafea8f0d3f089009ab01a7165a31d3b40
Furkan Kartal <Furkan.kartal@testinium.com>
2024-09-08 15:55:32 +0300

    deneme

--- src/test/java/Testasd.java
+++ src/test/java/Testasd.java
@@ -1,16 +1,19 @@
 import com.Testinium.Mobile.HookImpl;
 import com.Testinium.Mobile.StepImpl;
+import io.appium.java_client.AppiumDriver;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 public class Testasd extends HookImpl {
 
     StepImpl step= new StepImpl();
+    private AppiumDriver driver;
 
     @Test
     public void success(){
 
         step.waitBySecond(3);
-
+        driver.findElement(By.xpath("/hierarchy/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.FrameLayout[2]/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ImageView/android.widget.ImageView")).click();
     }
 
 }
