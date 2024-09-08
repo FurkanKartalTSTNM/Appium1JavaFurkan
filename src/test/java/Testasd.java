862c269aad0280d6ded57b79dcfe3eba114eff78
Furkan Kartal <Furkan.kartal@testinium.com>
2024-09-08 15:57:20 +0300

    deneme

--- src/test/java/Testasd.java
+++ src/test/java/Testasd.java
@@ -13,7 +13,7 @@ public class Testasd extends HookImpl {
     public void success(){
 
         step.waitBySecond(3);
-        driver.findElement(By.xpath("/hierarchy/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.FrameLayout[2]/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ImageView/android.widget.ImageView")).click();
+        driver.findElement(By.xpath("//*[contains(@resource-id, 'com.gratis.android:id/navigation_bar_item_icon_view')]")).click();
     }
 
 }
