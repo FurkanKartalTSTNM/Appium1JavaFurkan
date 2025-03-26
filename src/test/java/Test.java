7caf90afa51fe333611f364996287137019f63e5
Furkan Kartal <Furkan.kartal@testinium.com>
2025-03-26 14:00:13 +0300

    First commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,15 +1,13 @@
 import com.Testinium.Mobile.HookImpl;
 import com.Testinium.Mobile.StepImpl;
 import io.appium.java_client.AppiumDriver;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
-public class Testasd extends HookImpl {
+public class Test extends HookImpl {
 
     StepImpl step= new StepImpl();
     private AppiumDriver driver;
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void success() throws InterruptedException {
 
         step.waitBySecond(3);
